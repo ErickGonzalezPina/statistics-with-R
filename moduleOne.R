9d4c3bcbf68212014e3e9f8e7ffff41a8b5811d3
ErickGonzalezPina <erickbgonzalezp10@gmail.com>
2026-02-02 17:03:01 -0500

    Add datasets

--- moduleOne.R
+++ moduleOne.R
@@ -1,5 +1,5 @@
 
-# MODULE 1 TOPIC 2 ####
+# MODULE 1 TOPIC 2: Intro to R ####
 
 # 1.2 puzzle #1
 a = 10
@@ -12,7 +12,7 @@ result = abs(round(-1.23456789, 4))
 
 
 
-# MODULE 1 TOPIC 3 ####
+# MODULE 1 TOPIC 3: Vectors ####
 
 # How to create a vector and provide it a variable name
 # Modify a vector by adding new items to it
@@ -106,24 +106,62 @@ x[(x > 4) & (x < 8)]
 x[(x < 4) | (x >= 8)]
 
 
-# MODULE 1 TOPIC 4 ####
+# MODULE 1 TOPIC 4: Dataframes ####
 
 # 1. Upload a csv file to R
 # 2. A few functions on dataFrames
 # 3. Subset dataFrames
 
+--------------------------------------------------------------------------------
 # Open Census.csv in R
-census = read.csv("census.csv", stringsAsFactors = TRUE)
+census = read.csv("data/census.csv", stringsAsFactors = TRUE)
 
 # Look at the dataFrame
 census       # the wrong way
 View(census) # the right way
 
+--------------------------------------------------------------------------------
+# Create our own dataframe
+site = c('site1', 'site2', 'site3')
+counts = c(53,67,89)
+date = c('2025-10-03', '2025-10-05', '2025-10-07')
 
+df = data.frame(site, counts, date)
+View(df) 
 
+counts2 = c(10, 12, 0)
+date2 = c('2026-01-06', '2026-01-07', '2026-01-10')
+df = data.frame(df, date2, counts2)
+View(df)
 
+--------------------------------------------------------------------------------
+# Subsetting dataframes
+census[3,4] # return 3rd row 4th column
+census[3,]  # return the entire 3rd row and all columns
+census[,4]# return column 4 all rows
 
+# Syntactic Sugar - Refer to a single column by name
+census$age
+census$race_general
 
+--------------------------------------------------------------------------------
+# Puzzle #2: Give the variable name age to the age column. This is a vector. 
+  # Then print out just the 3rd entree of that vector.
+age = census$age
+age[3]
 
+--------------------------------------------------------------------------------
+# Return rows based on properties
+census_above_40 = census[census$age > 40, ] # Return rows where age is above 40
+View(census_above_40 )
+
+census_black = census[census$race_general == 'Black', ] # Return rows where race is black
+View(census_black)
+
+census$total_family_income[ census$age > 40] # Return vector of total family income with age above 40
+
+--------------------------------------------------------------------------------
+# Puzzle - Why is there an error?
+census$race_general[ censussd]
 
 
